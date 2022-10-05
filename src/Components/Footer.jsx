b4855e2fa0f516de6a9af662628f2b549ef84f90
HiagoP <hiagoperonip@gmail.com>
2022-10-05 13:47:40 -0300

    Att

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,7 +1,4 @@
 import React, { Component } from 'react'
-import linkedin from '../imagens/linkedin.png'
-import github from '../imagens/github.png'
-import instagram from '../imagens/instagram.png'
 
 
 class Footer extends Component {
@@ -10,15 +7,12 @@ class Footer extends Component {
       <footer>
         <p>Contatos:</p>
         <a rel="noreferrer" href="https://www.linkedin.com/in/hiago-peroni/" target="_blank">
-          {/* <img href={linkedin} alt="linkedin" /> */}
           Linkedin
         </a>
         <a rel="noreferrer" href="https://www.instagram.com/hiagoperonidev/" target="_blank">
-          {/* <img href={instagram} alt="instagram" /> */}
           Instagram
         </a>
         <a rel="noreferrer" href="https://github.com/Hiagoperoni" target="_blank">
-          {/* <img href={github} alt="github" /> */}
           GitHub
         </a>
         <p>E-mail: hiagoperonidev@gmail.com</p>
